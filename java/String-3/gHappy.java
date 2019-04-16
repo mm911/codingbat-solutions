c864d31cbf49a97bd643432a3c1e098d5603fff4
anddever <anddever@gmail.com>
2019-04-16 09:26:02 +0700

    Update gHappy.java
    Existing solution of mm911 don't cover use case where single 'g' appears before double 'g' etc.

--- java/String-3/gHappy.java
+++ java/String-3/gHappy.java
@@ -1,16 +1,12 @@
 public boolean gHappy(String str) {
-  int len = str.length();
-  boolean happy = true;
-
-  for (int i = 0; i < len; i++) {
+  if (str.length() == 1) return false;
+  for (int i = 0; i<str.length(); ++i)
     if (str.charAt(i) == 'g') {
-      if (i > 0 && str.charAt(i-1) == 'g')
-        happy = true;
-      else if (i < len-1 && str.charAt(i+1) == 'g')
-        happy = true;
-      else
-        happy = false;
+      if (i>0 && i<str.length()-1 && str.charAt(i-1) != 'g' &&
+        str.charAt(i+1) != 'g')
+        return false;
+      if (i == str.length()-1 && str.charAt(i-1) != 'g')
+        return false;
     }
-  }
-  return happy;
+  return true;
 }
